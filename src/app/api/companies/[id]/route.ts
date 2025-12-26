771f11437d42962f83c61d5b46a8846fa09b7643
xboikom1 <Maksimboiko222@gmail.com>
2025-12-26 07:08:10 +0100

    fix type error

--- src/app/api/companies/[id]/route.ts
+++ src/app/api/companies/[id]/route.ts
@@ -1,14 +1,13 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { Company } from '@/src/lib/api';
 import { getDb } from '@/src/lib/mongodb';
 
 export async function GET(
-  _request: Request,
-  { params }: { params: { id: string } },
+  request: NextRequest,
+  context: { params: { id: string } },
 ) {
   const db = await getDb();
-  const url = new URL(_request.url);
-  const id = params?.id ?? url.pathname.split('/').pop() ?? '';
+  const id = context.params.id;
 
   const company = await db
     .collection<Company>('companies')
