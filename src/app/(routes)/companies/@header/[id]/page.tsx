722f43f718c9e230fda1ab80d70b7fc0d7daceae
xboikom1 <Maksimboiko222@gmail.com>
2025-12-27 20:12:09 +0100

    fix company header

--- src/app/(routes)/companies/@header/[id]/page.tsx
+++ src/app/(routes)/companies/@header/[id]/page.tsx
@@ -1,14 +1,19 @@
 import Header from '@/src/app/components/header';
 import { getCompany } from '@/src/lib/api';
+import { useQuery } from '@tanstack/react-query';
 
 export interface PageProps {
   params: { id: string };
 }
 
-export default async function Page({ params }: PageProps) {
-  const { id } = await params;
-  const company = await getCompany(id, { cache: 'no-store' });
-  const title = company?.title ?? '';
+export default function Page({ params }: PageProps) {
+  const { id } = params;
+  const { data: company } = useQuery({
+    queryKey: ['companies', id],
+    queryFn: () => getCompany(id, { cache: 'no-store' }),
+  });
 
-  return <Header>{`Company (${title})`}</Header>;
+  return (
+    <Header>{`Company ${company?.title && `(${company?.title})`}`}</Header>
+  );
 }
