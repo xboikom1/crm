e34de0a903191dd3195a0b26f1990b96b84c55fe
xboikom1 <Maksimboiko222@gmail.com>
2025-12-27 20:44:33 +0100

    log id in header

--- src/app/(routes)/companies/@header/[id]/page.tsx
+++ src/app/(routes)/companies/@header/[id]/page.tsx
@@ -7,12 +7,13 @@ export interface PageProps {
 
 export default async function Page({ params }: PageProps) {
   const { id } = params;
-
+  console.log('Rendering header for company id:', id);
   let title = 'Company ';
 
   const company = await getCompany(id, { cache: 'no-store' });
+  console.log('Company data fetched:', company);
   if (company?.title) {
-    title += company.title;
+    title += `(${company.title})`;
   }
 
   return <Header>{title}</Header>;
